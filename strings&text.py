5b14659aab3d1bc18b06797668675a83be703306
Amirul Islam <iamirulofficial@gmail.com>
2019-07-03 22:03:45 +0530

    String

---
+++ strings&text.py
@@ -0,0 +1,18 @@
+types_of_people = 10
+x =  f"There are {types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said : {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evolution = "Isn't that joke so funny? {}"
+
+print(joke_evolution.format(hilarious))
+
+print(x + y)
